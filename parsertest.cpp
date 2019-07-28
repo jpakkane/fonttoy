3e587c91145e1e236d2d6b581ffa6786f3a54ed6
Jussi Pakkanen <jpakkane@gmail.com>
2019-07-29 01:54:02 +0300

    The basics work, precedence is broken.

--- parsertest.cpp
+++ parsertest.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <variant>
+#include <unordered_map>
 #include <cstdio>
 #include <cassert>
 
@@ -61,7 +62,7 @@ const std::vector<TokenDefinition> token_rules{
 
 // clang-format on
 
-const char* token_name(const TokenType t) {
+const char *token_name(const TokenType t) {
     assert((int)t >= 0);
     assert((size_t)t < token_rules.size());
     return token_rules[(int)t].typestr;
@@ -149,10 +150,31 @@ enum class NodeType : char {
     empty,
 };
 
+const std::vector<const char *> node_names{
+    "id",
+    "number",
+    "assignment",
+    "plus",
+    "minus",
+    "multiply",
+    "divide",
+    "negate",
+    "parentheses",
+    "statement",
+    "empty",
+};
+
+const char *node_name(const NodeType t) {
+    assert((int)t >= 0);
+    assert((size_t)t < node_names.size());
+    return node_names[(int)t];
+
+}
+
 struct Node final {
     explicit Node(NodeType type, const Token &t)
-        : type(type), value(std::monostate()), left{}, right{}, line_number(t.line_number), column_number(t.column_number) {
-    }
+        : type(type), value(std::monostate()), left{}, right{}, line_number(t.line_number),
+          column_number(t.column_number) {}
     NodeType type;
     std::variant<std::monostate, double, std::string> value;
     std::optional<int> left;
@@ -164,7 +186,7 @@ struct Node final {
 class Parser final {
 
 public:
-    Parser(Lexer &l) : l(l) {};
+    Parser(Lexer &l) : l(l){};
 
     bool parse() {
         assert(nodes.size() == 0);
@@ -187,43 +209,101 @@ public:
 
     const std::string get_error() const { return error_message; }
 
+    const std::vector<Node> &get_nodes() const { return nodes; }
+    const std::vector<int> &get_statements() const { return statements; }
+
 private:
     bool is_error() const { return !error_message.empty(); }
 
     bool e1_statement() {
-        if(t.type != TokenType::id) {
-            error_message = "Incorrect node type for statement: ";
-            error_message += token_name(t.type);
-            return false;
-        }
-        auto id_index = nodes.size();
-        nodes.emplace_back(NodeType::id, t);
-        nodes.back().value = t.contents;
-        if(!expect(TokenType::equal)) {
-            return false;
-        }
-        auto assignment_index = nodes.size();
-        nodes.emplace_back(NodeType::assignment, t);
-        statements.push_back(assignment_index);
         if(!e2_arithmetic()) {
             return false;
         }
+        if(nodes.empty()) {
+            set_error("Something really weird has happened.", -1, -1);
+            return false;
+        }
+        if(nodes.back().type != NodeType::id) {
+            std::string err = "Incorrect node type for statement: ";
+            err += token_name(t.type);
+            set_error(err.c_str(), nodes.back().line_number, nodes.back().column_number);
+            return false;
+        }
+        if(!expect(TokenType::equal)) {
+            set_error("Missing equals sign", t.line_number, t.column_number);
+            return false;
+        }
+        int id_index = nodes.size()-1;
+        if(!e2_arithmetic()) {
+            return false;
+        }
+        int val_index = nodes.size()-1;
+        nodes.emplace_back(NodeType::assignment, t);
+        statements.push_back(nodes.size()-1);
+        nodes.back().left = id_index;
+        nodes.back().right = val_index;
         if(!expect(TokenType::linefeed)) {
             return false;
         }
-        nodes[assignment_index].left = id_index;
-        nodes[assignment_index].right = nodes.size()-1;
-
         return true;
     }
 
     bool e2_arithmetic() {
-        // FIXME: add implementation here.
-        return false;
+        return(e3_multiply());
     }
 
-    bool e3_parentheses() {
-        if(!e4_token()) {
+    bool e3_multiply() {
+        if(!e4_divide()) {
+            return false;
+        }
+        Token mul_token = t;
+        if(accept(TokenType::multiply)) {
+            int left = nodes.size() - 1;
+            if(!e3_multiply()) {
+                return false;
+            }
+            int right = nodes.size() - 1;
+            nodes.emplace_back(NodeType::multiply, mul_token);
+            nodes.back().left = left;
+            nodes.back().right = right;
+        }
+        return true;
+    }
+
+    bool e4_divide() {
+        if(!e5_add()) {
+            return false;
+        }
+        return true;
+    }
+
+    bool e5_add() {
+        if(!e6_subtract()) {
+            return false;
+        }
+        Token add_token = t;
+        if(accept(TokenType::plus)) {
+            int left = nodes.size() - 1;
+            if(!e5_add()) {
+                return false;
+            }
+            int right = nodes.size() - 1;
+            nodes.emplace_back(NodeType::plus, add_token);
+            nodes.back().left = left;
+            nodes.back().right = right;
+        }
+        return true;
+    }
+
+    bool e6_subtract() {
+        if(!e7_parentheses()) {
+            return false;
+        }
+        return true;
+    }
+
+    bool e7_parentheses() {
+        if(!e9_token()) {
             return false;
         }
         if(accept(TokenType::lparen)) {
@@ -237,7 +317,7 @@ private:
         return true;
     }
 
-    bool e4_token() {
+    bool e9_token() {
         auto current_token = t;
         if(accept(TokenType::id)) {
             nodes.emplace_back(NodeType::id, current_token);
@@ -250,10 +330,13 @@ private:
             nodes.back().value = strtod(current_token.contents.c_str(), nullptr);
             return true;
         }
+        nodes.emplace_back(NodeType::empty, t);
+        /*
         std::string err{"Got unexpected token: "};
         err += token_name(t.type);
         set_error(err.c_str(), t.line_number, t.column_number);
-        return false;
+        */
+        return true;
     }
 
     bool accept(const TokenType type) {
@@ -293,18 +376,104 @@ private:
     std::string error_message;
 };
 
+class Interpreter final {
+public:
+
+    explicit Interpreter(const Parser &p) : nodes(p.get_nodes()), statements(p.get_statements()) {}
+
+    bool execute_program() {
+        for(const auto statement: statements) {
+            const auto &n = nodes[statement];
+            if (n.type == NodeType::assignment) {
+                if(!assignment(n)) {
+                    return false;
+                }
+            } else {
+                std::string err("Unknown node type ");
+                err += node_name(n.type);
+                set_error(err, n.line_number, n.column_number);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    double get_variable(const char *varname) const {
+        return get_variable(std::string(varname));
+    }
+
+    double get_variable(const std::string &varname) const {
+        auto node = variables.find(varname);
+        assert(node != variables.end());
+        return node->second;
+    }
+
+    const std::string& get_error() const { return error_message; }
+
+private:
+
+    void set_variable(const std::string &name, double value) {
+        variables[name] = value;
+    }
+
+    bool assignment(const Node &n) {
+        assert(n.type == NodeType::assignment);
+        const std::string &varname = std::get<std::string>(nodes[n.left.value()].value);
+        double value = expression(nodes[n.right.value()]);
+        printf("Setting variable %s to %f\n", varname.c_str(), value);
+        set_variable(varname, value);
+        return true;
+    }
+
+    double expression(const Node &n) {
+        if(n.type == NodeType::number) {
+            return std::get<double>(n.value);
+        } else if(n.type == NodeType::multiply) {
+            double lval = expression(nodes[n.left.value()]);
+            double rval = expression(nodes[n.right.value()]);
+            return lval*rval;
+        } else if(n.type == NodeType::plus) {
+            double lval = expression(nodes[n.left.value()]);
+            double rval = expression(nodes[n.right.value()]);
+            return lval+rval;
+        } else {
+            printf("Unknown node type: %s\n", node_name(n.type));
+            return 0.0;
+        }
+    }
+
+    void set_error(const char *msg, int line_number, int column_number) {
+        assert(error_message.empty());
+        error_message = std::to_string(line_number);
+        error_message += ':';
+        error_message += std::to_string(column_number);
+        error_message += ' ';
+        error_message += msg;
+    }
+
+    void set_error(const std::string &msg, int line_number, int column_number) {
+        set_error(msg.c_str(), line_number, column_number);
+    }
+
+    const std::vector<Node> &nodes;
+    const std::vector<int> &statements;
+    std::string error_message;
+    std::unordered_map<std::string, double> variables;
+
+};
+
 int main(int, char **) {
     std::string input("x = 1.0 + 2.0*3");
     Lexer tokenizer(input);
-    Token t = tokenizer.next();
-    while(t.type != TokenType::eof && t.type != TokenType::error) {
-        printf("Token: %s\n", token_name(t.type));
-        printf(" value: \"%s\"\n", t.contents.c_str());
-        printf(" line: %d\n col: %d\n", t.line_number, t.column_number);
-        t = tokenizer.next();
+    Parser p(tokenizer);
+    if(!p.parse()) {
+        printf("Parser error: %s\n", p.get_error().c_str());
+        return 1;
     }
-    if(t.type == TokenType::error) {
-        printf("An error occurred: %s\n", t.contents.c_str());
+    Interpreter i(p);
+    if(!i.execute_program()) {
+        printf("Interpreter error: %s\n", i.get_error().c_str());
+        return 1;
     }
     return 0;
 }
